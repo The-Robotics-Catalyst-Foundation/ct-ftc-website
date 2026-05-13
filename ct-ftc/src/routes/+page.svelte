5ee40923cd3420b2cf08dacb02361d67646e6aed
SID AG <agsid@gmail.com>
2026-05-13 16:06:43 -0400

    go back to svelte

---
+++ ct-ftc/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="bg-gray-400 min-h-screen flex flex-col items-center">
+<div class="bg-gray-300 w-full h-full p-50 justify-center items-center"><h1 class="pt-10 text-4xl">Welcome to the CT FTC website!</h1></div>
+</div>
